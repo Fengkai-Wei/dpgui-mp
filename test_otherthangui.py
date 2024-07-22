24a49007c8847231b2fa82b09791edc1cf967a2c
Fengkai-Wei <fkpc101@fkpc101.ads.uni-jena.de>
2024-07-22 18:12:54 +0200

    Three_views

--- test_otherthangui.py
+++ test_otherthangui.py
@@ -2,59 +2,44 @@ import pyvista as pv
 import numpy as np
 import time
 
-reso = 50
-start_time = time.time()
+reso = 15
+
 
 # 创建球体
 sphere = pv.Sphere(radius=1.0, center=(0, 0, 0), theta_resolution=reso,phi_resolution=reso)
 
-time_sphere = time.time()
+
 
 # 创建立方体
 cube = pv.Cube(center=(2, 0, 0))
-time_cube = time.time()
+
 # 创建圆柱体
 cylinder = pv.Cylinder(radius=0.5, height=2.0, center=(-2, 0, 0),resolution=reso)
-time_cylinder = time.time()
 
 # 合并形状
 combined = sphere + cube + cylinder
-time_combine = time.time()
 
+def sort_vertices(vertices):
+    # 计算质心
+    centroid = np.mean(vertices, axis=0)
+    
+    # 计算每个顶点相对于质心的角度
+    def angle_from_centroid(vertex):
+        return np.arctan2(vertex[1] - centroid[1], vertex[0] - centroid[0])
+    
+    # 对顶点按角度进行排序
+    sorted_vertices = sorted(vertices, key=angle_from_centroid)
+    return np.array(sorted_vertices)
 
-# 在z=0位置进行切片
-slice_z = combined.slice(normal='z', origin=(0, 0, 0))
-slice_time = time.time()
+def vista2dpg_plot(sliced_list :list, norm):
 
-# 获取切片后顶点坐标
-vertices = slice_z.points
-
-get_ver_time = time.time()
-
-print(f'resolution = {reso}')
-print(f'create sphere : {time_sphere - start_time}')
-print(f'create cube : {time_cube - time_sphere}')
-print(f'create cylinder : {time_cylinder - time_cube}')
-print(f'combine shape: {time_combine - time_cylinder}')
-print(f'create slice : {slice_time - time_combine}')
-print(f'get vertices : {get_ver_time - slice_time}')
-
-"""# 打印顶点坐标
-print("Slice at z=0 coordinates:")
-print(vertices)"""
-
-# 可视化原始组合体和切片
-plotter = pv.Plotter(shape=(1, 2))
-
-# 添加原始组合体
-plotter.subplot(0, 0)
-plotter.add_mesh(combined, color='white', show_edges=True, opacity=0.5)
-plotter.add_text('Combined Shape', font_size=10)
-
-# 添加切片
-plotter.subplot(0, 1)
-plotter.add_mesh(slice_z, color='red', show_edges=True)
-plotter.add_text('Slice at z=0', font_size=10)
-
-# 显示结果
-plotter.show()
+    sorted_list = []
+    for sliced_points in sliced_list:
+        flatten  = np.delete(arr=sliced_points, obj= norm,axis= 1)
+        sorted = sort_vertices(flatten)
+        
+        sorted_list.append(sorted.transpose().tolist())
+    return sorted_list
+    
+a = vista2dpg_plot([cylinder.slice(normal='z', origin=(0, 0, 0)).points],2)
+print(a)
